f6d82a384cc1c09ee6b486bce54b5fd70e88c652
Tuyetanh090720 <ngothituyetanhbmt@gmail.com>
2022-08-18 13:49:11 +0700

    edit frontend

---
+++ resources/views/clients/calendar.blade.php
@@ -0,0 +1,56 @@
+
+<div class="calendar">
+    <div class="month">
+        <ul>
+            <li class="prev">&#10094;</li>
+            <li class="next">&#10095;</li>
+            <li style="text-align:center">
+            Tháng 5, 2017
+            </li>
+        </ul>
+    </div>
+
+    <ul class="weekdays">
+        <li>CN</li>
+        <li>T2</li>
+        <li>T3</li>
+        <li>T4</li>
+        <li>T5</li>
+        <li>T6</li>
+        <li>T7</li>
+    </ul>
+
+    <ul class="days">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li><span class="active">24</span></li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+    </ul>
+</div>
